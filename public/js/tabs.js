af7c50076b70ca907f793332d6beb0aedf6bc9d1
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-05-15 19:52:10 -0700

    finish tabs component, update type and spacing, and streamline content-grid.scss

--- public/js/tabs.js
+++ public/js/tabs.js
@@ -1,20 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tabButtons = document.querySelectorAll('.tab-button');
-    const tabContents = document.querySelectorAll('.tab-content');
-  
-    tabButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const targetTab = this.getAttribute('data-tab');
-        tabButtons.forEach(btn => btn.classList.remove('active'));
-        tabContents.forEach(content => {
-          if (content.id === targetTab) {
-            content.classList.add('active');
-          } else {
-            content.classList.remove('active');
-          }
-        });
-        this.classList.add('active');
+  const tabButtons = document.querySelectorAll('.tab-button');
+  const tabContents = document.querySelectorAll('.tab-content');
+  const tabsMenu = document.querySelector('.tabs-menu'); // Add this line
+
+  tabButtons.forEach(button => {
+    button.addEventListener('click', function () {
+      const targetTab = this.getAttribute('data-tab');
+      tabButtons.forEach(btn => btn.classList.remove('active'));
+      tabContents.forEach(content => {
+        if (content.id === targetTab) {
+          content.classList.add('active');
+        } else {
+          content.classList.remove('active');
+        }
       });
+      this.classList.add('active');
+
+      // Calculate the offsetLeft of the clicked tab button and adjust the tabsMenu scrollLeft
+      const offsetLeft = this.offsetLeft;
+      tabsMenu.scrollLeft = offsetLeft - tabsMenu.offsetWidth / 4;
     });
   });
-  
+});
