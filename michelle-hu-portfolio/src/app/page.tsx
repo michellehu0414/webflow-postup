f68e665cd7a6090c015cfe67b292bf87addc5ba8
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-06-17 16:36:42 -0700

    begin converting old code and structure to next.js, and create Accordion component

---
+++ michelle-hu-portfolio/src/app/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Accordion from "@/components/Accordion";
+
+export default function Home() {
+  const [open, setOpen] = useState(false);
+  return (
+    <main>
+      <Accordion open={open} setOpen={setOpen} label="Header">
+        <p>Content</p>
+      </Accordion>
+    </main>
+  );
+}
---
+++ michelle-hu-portfolio/src/components/Accordion/index.tsx
@@ -0,0 +1,47 @@
+import { Dispatch, SetStateAction, ReactNode } from 'react';
+import Image from 'next/image';
+import styles from './styles.module.scss';
+import chevron from "public/assets/svg/ic-chevron-arrow__down--white.svg";
+
+export type AccordionProps = {
+	open: boolean;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+	children: ReactNode;
+	label: string;
+};
+
+/**
+ * Accordion component that toggles its content when the header is clicked.
+ * @param open - State that determines if the content is visible.
+ * @param setOpen - Function that toggles the open state.
+ * @param children - Content to be displayed when the accordion is open.
+ * @param label - Label for the accordion header.
+ * @returns Accordion component.
+ */
+export default function Accordion({
+	open,
+	setOpen,
+	children,
+	label,
+}: AccordionProps) {
+	const toggleOpen = () => setOpen((prev) => !prev);
+
+	return (
+		<div className={styles.container}>
+			<button
+				className={styles.header}
+				onClick={toggleOpen}
+				aria-expanded={open}>
+				<p className={styles.label}>{label}</p>
+				<Image
+					className={`${styles.icon} ${open ? styles.rotate : ''}`}
+          src={chevron}
+					alt={`Chevron arrow pointing ${open ? 'up' : 'down'}`}
+					width={24}
+					height={24}
+				/>
+			</button>
+			{open && <div className={styles.content}>{children}</div>}
+		</div>
+	);
+}
