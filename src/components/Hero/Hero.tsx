6c50c38cff5b116547fbbf930ce5409bdf7c492f
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-06-18 23:20:18 -0700

    begin creating global Hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { AppProps } from "next/app";
+import styles from "./Hero.module.scss";
+import "@/styles/globals.scss";
+import Image from "next/image";
+
+const Hero: React.FC = () => {
+	return (
+		<section
+			id='hero'
+			className='hero hero--bg-tastebuds'>
+			<div className='hero__content'>
+				<img
+					alt=''
+					className='hero__image'
+					loading='eager'
+					src='assets/images/tastebuds/hero-image.png'
+				/>
+				{/* <div className="hero__image-wrapper">
+                </div> */}
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
