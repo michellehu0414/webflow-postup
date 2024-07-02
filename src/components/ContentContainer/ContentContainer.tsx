137264ffb40edf5d2e6619bc60d90cefa880eb12
Brenden Smith <git@brenden-smith.com>
2024-07-02 01:11:40 -0700

    Update component definitions

--- src/components/ContentContainer/ContentContainer.tsx
+++ src/components/ContentContainer/ContentContainer.tsx
@@ -1,21 +1,30 @@
 // components/ContentContainer.tsx
-import React, { ReactNode } from 'react';
-import styles from './ContentContainer.module.scss';
+import React, { ReactNode } from "react";
+import styles from "./ContentContainer.module.scss";
 
 type ContentContainerProps = {
-	size?: 'medium-small' | 'medium' | 'large' | 'xl' | 'xxl';
-	children: ReactNode;
+  size?: "medium-small" | "medium" | "large" | "xl" | "xxl";
+  children: ReactNode;
 };
 
-const ContentContainer: React.FC<ContentContainerProps> = ({
-	size,
-	children,
-}) => {
-	const sizeClass = size ? styles[size] : '';
+// const ContentContainer: React.FC<ContentContainerProps> = ({
+// 	size,
+// 	children,
+// }) => {
+// 	const sizeClass = size ? styles[size] : '';
 
-	return (
-		<div className={`${styles['container']} ${sizeClass}`}>{children}</div>
-	);
-};
+// 	return (
+// 		<div className={`${styles['container']} ${sizeClass}`}>{children}</div>
+// 	);
+// };
 
-export default ContentContainer;
+export default function ContentContainer({
+  size,
+  children,
+}: ContentContainerProps) {
+  const sizeClass = size ? styles[size] : "";
+
+  return (
+    <div className={`${styles["container"]} ${sizeClass}`}>{children}</div>
+  );
+}
