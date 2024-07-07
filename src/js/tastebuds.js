50a2de8adb2c0e3640a9d4d0ff6cdcf237bca114
michellehu0414 <103789293+michellehu0414@users.noreply.github.com>
2024-07-07 02:18:08 -0700

    feat(MediaCarousel): Create MediaCarousel component JS and SCSS files and import to tastebuds.js

--- src/js/tastebuds.js
+++ src/js/tastebuds.js
@@ -3,11 +3,18 @@ import '../scss/tastebuds.scss';
 
 // Import and initialize components specific to this page
 import '../components/Accordion/accordion';
+// import '../components/Carousel/carousel';
 import '../components/AutoplayVideo/autoplay-video';
 import '../components/AutoplayVideoCarousel/AutoplayVideoCarousel'
 import '../components/ScrollButton/scroll-button';
 import '../components/SequentialVideoPlayer/sequential-video-player';
 import '../components/Tabs/tabs';
+
+import MediaCarousel from '../components/MediaCarousel/MediaCarousel';
+document.addEventListener('DOMContentLoaded', function () {
+    new MediaCarousel('#carousel-01');
+    new MediaCarousel('#carousel-02');
+});
 
 import '../components/scrollable';
 
