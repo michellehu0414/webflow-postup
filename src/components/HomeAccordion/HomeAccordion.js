11b866b2f51dcd2497cdb0228778b4521bec1289
mchll.h <michellehu0414@users.noreply.github.com>
2024-07-10 02:05:36 -0700

    Fix HomeAccordion component and relocate SCSS style from index.scss to home-components.scss to streamline index.scss

--- src/components/HomeAccordion/HomeAccordion.js
+++ src/components/HomeAccordion/HomeAccordion.js
@@ -1,20 +1,21 @@
-document.querySelectorAll('.accordionHeader').forEach(button => {
-    button.addEventListener('click', () => {
-        const accordionContent = button.nextElementSibling;
-        const arrow = button.querySelector('.arrow');
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.accordionHeader').forEach(button => {
+        button.addEventListener('click', () => {
+            const accordionContent = button.nextElementSibling;
+            const arrow = button.querySelector('.accordion-arrow');
 
-        // Check if accordion is currently open
-        if (button.classList.contains('active')) {
-            // It's open, so close it
-            accordionContent.style.maxHeight = null; // Set maxHeight to 0 effectively
-            accordionContent.style.maxHeight = '0'; // Ensure collapsing animation
-            arrow.style.transform = 'rotate(0deg)';
-            button.classList.remove('active'); // Remove the active class if content is being hidden
-        } else {
-            // It's closed, so open it
-            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px"; // Set maxHeight to the scrollHeight for expanding animation
-            arrow.style.transform = 'rotate(-180deg)';
-            button.classList.add('active'); // Add the active class if content is being shown
-        }
+            // Check if accordion is currently open
+            if (button.classList.contains('active')) {
+                // It's open, so close it
+                accordionContent.style.maxHeight = '0'; // Ensure collapsing animation
+                arrow.style.transform = 'rotate(0deg)';
+                button.classList.remove('active'); // Remove the active class if content is being hidden
+            } else {
+                // It's closed, so open it
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + "px"; // Set maxHeight to the scrollHeight for expanding animation
+                arrow.style.transform = 'rotate(-180deg)';
+                button.classList.add('active'); // Add the active class if content is being shown
+            }
+        });
     });
 });
